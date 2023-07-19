02158ea29b5fbbc8bfb4c8b22f79f5c633b98672
Shashatkin Anton <shashatkin@mail.ru>
2023-07-19 19:17:59 +0400

    Html templates have been added and bootstrap styles have been connected.

--- main.go
+++ main.go
@@ -2,30 +2,36 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, happiness float64
+	Hobbies               []string
 }
 
 func (self *User) getAllInfo() string {
 	return fmt.Sprintf("User name is: %s. He is %d "+
-		"and he has money equal: %d", self.name, self.age, self.money)
+		"and he has money equal: %d", self.Name, self.Age, self.Money)
 }
 
 func (self *User) setNewName(nname string) {
-	self.name = nname
+	self.Name = nname
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
 
-	bob := User{"Bob", 25, -50, 4.2, 0.8}
-	bob.setNewName("Anton")
-	fmt.Fprintf(w, bob.getAllInfo())
+	bob := User{"Bob", 25, -50, 4.2, 0.8, []string{"Fottball", "Skate", "Dance"}}
+	//bob.setNewName("Anton")
+	//fmt.Fprintf(w, bob.getAllInfo())
+
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+
+	tmpl.Execute(w, bob)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
